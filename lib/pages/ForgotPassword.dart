29168f34b301323e5110bbb509823957708972a4
arifah-21 <arifah98@graduate.utm.my>
2022-12-02 18:23:33 +0800

    new update

---
+++ lib/pages/ForgotPassword.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatelessWidget {
+
+  static String id = 'forgot password';
+
+  BuildContext? get context => null;
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Form(
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Email your Email',
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                    TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                            labelText: 'Email',
+                            icon: Icon(
+                              Icons.import_contacts_sharp,
+                              color: Colors.white,
+                            ),
+                            errorStyle: TextStyle(color: Colors.white),
+                            labelStyle: TextStyle(color: Colors.white),
+                            hintStyle: TextStyle(color: Colors.white),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            errorBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            )
+                        )
+
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10,
+                  ),
+                Visibility(
+                  maintainSize: true,
+                   maintainAnimation: true,
+                   maintainState: true,
+                    visible: visible,
+                   child: Container(
+                      child: CircularProgressIndicator(
+                      color: Colors.white,
+                      ))),
+          ],
+        ),
+    );
+  }
+
+  void Forgotpassss(String email) async {
+    if (_formkey.currentState!.validate()) {
+      await _auth
+          .sendPasswordResetEmail(email: email)
+          .then((uid) => {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => LoginPage()))
+      })
+          .catchError((e) {});
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+

4b62af39e57f421b9cd044c3b4a6593ac5afc672
IrsyadRosHisyamRos <irsyadros@graduate.utm.my>
2022-12-02 22:57:23 +0800

    Adjusted the login page layout to be flexible for other aspect ratios

--- lib/pages/signInPage.dart
+++ lib/pages/signInPage.dart
@@ -45,178 +45,182 @@ class _SignInPageState extends State<SignInPage> {
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
         child: Form(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              SizedBox(height: 50.0),
+              SizedBox(height: 35.0),
               Image.asset(
                 "images/title.png",
                 width: double.infinity,
               ),
-              SizedBox(height: 50.0),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  color: Color(0xff26201d),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0xffd8ae8b),
-                      blurRadius: 5.0,
-                      spreadRadius: 1.0,
-                    )
-                  ],
-                ),
-                child: TextFormField(
-                  decoration: new InputDecoration(
-                    hintText: 'Email or Phone Number',
-                    contentPadding: EdgeInsets.only(left: 30.0, right: 30.0, top: 20.0, bottom: 20.0),
-                    hintStyle: TextStyle(
-                        fontFamily: 'DMSans',
-                        fontSize: 16.0,
-                        color: Color(0xaafaf5d8),
-                    ),
-                    fillColor: Colors.white,
-                    focusedBorder: OutlineInputBorder(
+              SizedBox(height: 35.0),
+              Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(15.0),
-                      borderSide: BorderSide(
-                        color: Color(0xfff2ab37),
-                        width: 4.0,
+                      color: Color(0xff26201d),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xffd8ae8b),
+                          blurRadius: 5.0,
+                          spreadRadius: 1.0,
+                        )
+                      ],
+                    ),
+                    child: TextFormField(
+                      decoration: new InputDecoration(
+                        hintText: 'Email or Phone Number',
+                        contentPadding: EdgeInsets.only(left: 30.0, right: 30.0, top: 20.0, bottom: 20.0),
+                        hintStyle: TextStyle(
+                          fontFamily: 'DMSans',
+                          fontSize: 16.0,
+                          color: Color(0xaafaf5d8),
+                        ),
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: BorderSide(
+                            color: Color(0xfff2ab37),
+                            width: 4.0,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: BorderSide(
+                            color: Color(0xffd8ae8b),
+                            width: 2.0,
+                          ),
+                        ),
                       ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                      borderSide: BorderSide(
-                        color: Color(0xffd8ae8b),
-                        width: 2.0,
-                      ),
-                    ),
-                  ),
-                  style: TextStyle(
-                    color: Color(0xfffaf5d8),
-                  ),
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  color: Color(0xff26201d),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0xffd8ae8b),
-                      blurRadius: 5.0,
-                      spreadRadius: 1.0,
-                    )
-                  ],
-                ),
-                child: TextFormField(
-                  decoration: new InputDecoration(
-                    hintText: 'Password',
-                    contentPadding: EdgeInsets.only(left: 30.0, right: 30.0, top: 20.0, bottom: 20.0),
-                    hintStyle: TextStyle(
-                        fontFamily: 'DMSans',
-                        fontSize: 16.0,
-                        color: Color(0xaafaf5d8),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                      borderSide: BorderSide(
-                        color: Color(0xfff2ab37),
-                        width: 4.0,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                      borderSide: BorderSide(
-                        color: Color(0xffd8ae8b),
-                        width: 2.0,
-                      ),
-                    ),
-                  ),
-                  style: TextStyle(
-                    color: Color(0xfffaf5d8),
-                  ),
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  gradient: LinearGradient(
-                    begin: Alignment(-0.95, 0.0),
-                    end: Alignment(1.0, 0.0),
-                    colors: [Color(0xffcd5f2a), Color(0xfff2ab37)],
-                    stops: [0.0, 1.0],
-                  ),
-                ),
-                child: SizedBox(
-                  height: 60.0,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                          )
-                      ),
-                      backgroundColor: MaterialStateProperty.resolveWith((states) {
-                        return Color(0x0000ff00);
-                      }),
-                      elevation: MaterialStateProperty.resolveWith((states) {
-                        return 0.0;
-                      }),
-                      overlayColor: MaterialStateProperty.resolveWith((states) {
-                        return states.contains(MaterialState.pressed) ? Color(0xaacd5f2a) : null;
-                      }),
-                    ),
-                    child: Text(
-                      'Sign In',
                       style: TextStyle(
-                        fontFamily: 'DMSans',
-                        fontWeight: FontWeight.w700,
-                        fontSize: 24.0,
                         color: Color(0xfffaf5d8),
                       ),
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
                     ),
-                    onPressed: () async {
-                      print("email: " + email + " | password: " + password);
-                    },
                   ),
-                )
-              ),
-              SizedBox(height: 10.0),
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Don\'t have an account? ',
+                  SizedBox(height: 20.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      color: Color(0xff26201d),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xffd8ae8b),
+                          blurRadius: 5.0,
+                          spreadRadius: 1.0,
+                        )
+                      ],
+                    ),
+                    child: TextFormField(
+                      decoration: new InputDecoration(
+                        hintText: 'Password',
+                        contentPadding: EdgeInsets.only(left: 30.0, right: 30.0, top: 20.0, bottom: 20.0),
+                        hintStyle: TextStyle(
+                          fontFamily: 'DMSans',
+                          fontSize: 16.0,
+                          color: Color(0xaafaf5d8),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: BorderSide(
+                            color: Color(0xfff2ab37),
+                            width: 4.0,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: BorderSide(
+                            color: Color(0xffd8ae8b),
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
                       style: TextStyle(
-                        fontFamily: 'DMSans',
-                        fontSize: 16.0,
                         color: Color(0xfffaf5d8),
                       ),
+                      obscureText: true,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
                     ),
-                    TextSpan(
-                      text: 'Sign up',
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        fontFamily: 'DMSans',
-                        fontSize: 16.0,
-                        color: Color(0xfff2ab37),
+                  ),
+                  SizedBox(height: 20.0),
+                  Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        gradient: LinearGradient(
+                          begin: Alignment(-0.95, 0.0),
+                          end: Alignment(1.0, 0.0),
+                          colors: [Color(0xffcd5f2a), Color(0xfff2ab37)],
+                          stops: [0.0, 1.0],
+                        ),
                       ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          print('User moved to sign up page');
-                        }
+                      child: SizedBox(
+                        height: 60.0,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                )
+                            ),
+                            backgroundColor: MaterialStateProperty.resolveWith((states) {
+                              return Color(0x0000ff00);
+                            }),
+                            elevation: MaterialStateProperty.resolveWith((states) {
+                              return 0.0;
+                            }),
+                            overlayColor: MaterialStateProperty.resolveWith((states) {
+                              return states.contains(MaterialState.pressed) ? Color(0xaacd5f2a) : null;
+                            }),
+                          ),
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              fontFamily: 'DMSans',
+                              fontWeight: FontWeight.w700,
+                              fontSize: 24.0,
+                              color: Color(0xfffaf5d8),
+                            ),
+                          ),
+                          onPressed: () async {
+                            print("email: " + email + " | password: " + password);
+                          },
+                        ),
+                      )
+                  ),
+                  SizedBox(height: 10.0),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Don\'t have an account? ',
+                          style: TextStyle(
+                            fontFamily: 'DMSans',
+                            fontSize: 16.0,
+                            color: Color(0xfffaf5d8),
+                          ),
+                        ),
+                        TextSpan(
+                            text: 'Sign up',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontFamily: 'DMSans',
+                              fontSize: 16.0,
+                              color: Color(0xfff2ab37),
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print('User moved to sign up page');
+                              }
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
               SizedBox(height: 20.0),
               Row(

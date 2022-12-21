02f7feb1b7c36bba1464f53b09f4d50f7481d4ef
Zirah11 <nurul.nazirah@graduate.utm.my>
2022-12-21 08:30:03 +0800

    reviewPage1.dart able to run

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: HomePage(),
+    return const MaterialApp(
+        home: ReviewPage(),
     );
   }
 }
--- lib/reviewCard.dart
+++ lib/reviewCard.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'review.dart';
 
+// ignore: camel_case_types
 class reviewCard extends StatelessWidget {
+  // ignore: non_constant_identifier_names
   final review Review;
   final Function() submit;
 
-  reviewCard({required this.Review, required this.submit});
+  // ignore: non_constant_identifier_names
+  const reviewCard({required this.Review, required this.submit});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        margin: const EdgeInsets.fromLTRB (16.0, 16.0, 16.0, 0),
+    return const Card(
+        margin: EdgeInsets.fromLTRB (16.0, 16.0, 16.0, 0),
         child: Padding(
-          padding: const EdgeInsets.all (12.0),
+          padding: EdgeInsets.all (12.0),
         )
     );
   }
